196ddab9c1894c4c504cf8f24d669f31bca8989f
Fede <fede.garza.ramirez@gmail.com>
2020-03-05 17:54:56 -0600

    train models

--- fforma/train_models.py
+++ fforma/train_models.py
@@ -16,24 +16,36 @@ class trainBasicModels:
 
         return fitted_model
 
-    def train(self, basic_models, ts_list, frcy):
+    def train(self, basic_models, y_train_df,  frcy):
         """
-        basic_models: List of models
+        basic_models: Dict of models with name
         """
+        ts_list = [df['y'].values for idx, df in y_train_df.groupby('unique_id')]
+        
+        self.model_names = basic_models.keys()
+        self.basic_models_list = basic_models.values()
+        
         self.fitted_models = [
-            np.array([self.train_basic(model, ts, frcy) for model in basic_models]) for ts in tqdm.tqdm(ts_list)
+            np.array([self.train_basic(model, ts, frcy) for model in self.basic_models_list]) for ts in tqdm.tqdm(ts_list)
         ]
 
         return self
 
-    def predict(self, h):
-        if not isinstance(h, list):
-            y_hat = [
-                np.array([model.predict(h) for model in idts]) for idts in tqdm.tqdm(self.fitted_models)
-            ]
-        else:
-            y_hat = [
-                np.array([model.predict(h[idh]) for model in idts]) for idh, idts in tqdm.tqdm(enumerate(self.fitted_models))
-            ]
-
-        return y_hat
+    def predict(self, y_hat_df):
+        
+        y_hat_r = y_hat_df.filter(items=['unique_id', 'ds'])
+        
+        h = y_hat_r.groupby('unique_id').size()[0]
+        
+        y_hat = [
+            np.array([model.predict(h) for model in idts]) for idts in tqdm.tqdm(self.fitted_models)
+        ]
+        
+        unique_ids = y_hat_df['unique_id'].unique()
+        
+        for idx, u_id in enumerate(unique_ids):
+            for id_model, model in enumerate(self.model_names):
+                y_hat_r.loc[y_hat_r['unique_id'] == u_id, f'y_{model}'] = y_hat[idx][id_model] 
+        
+        
+        return y_hat_r
