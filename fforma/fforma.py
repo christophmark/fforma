550b8ae3b1329175b9ec3edeb24aaa50e1bccff6
Federico Garza <fede.garza.ramirez@gmail.com>
2020-03-11 15:45:33 -0600

    Update fforma.py

--- fforma/fforma.py
+++ fforma/fforma.py
@@ -134,7 +134,7 @@ class FForma:
         grad = preds_transformed*(self.contribution_to_error[y, :] - weighted_avg_loss_func)
         hess = self.contribution_to_error[y,:]*preds_transformed*(1.0-preds_transformed) - grad*preds_transformed
         #print(grad)
-        return grad.reshape(-1, 1), hess.reshape(-1, 1)
+        return grad.flatten()/np.linalg.norm(grad), hess.flatten()/np.linalg.norm(hess)
 
     def fforma_loss(self, predt: np.ndarray, dtrain: xgb.DMatrix) -> (str, float):
         '''
