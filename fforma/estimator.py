3f2d391179cbd04b79732ec72368e4a0a9c58457
José Morales <jmorales@grupoabraxas.com>
2020-05-14 16:39:44 -0500

    remove y from fit method

--- fforma/estimator.py
+++ fforma/estimator.py
@@ -2,21 +2,17 @@ import pandas as pd
 import xgboost as xgb
 
 from sklearn.model_selection import train_test_split
-from tsfeatures import tsfeatures
 
 class FForma:
 
     def __init__(self):
         pass
 
-    def fit(self, errors, y, ts_feats):
-        # Construir lista de series
-        ts_list = [serie.values for _, serie in y.groupby('unique_id')]
-
+    def fit(self, ts_features, errors):
         # Entenar xgboost
         xgb_y = errors.values.argmin(axis=1)
 
-        X_train, X_valid, y_train, y_valid = train_test_split(ts_feats, xgb_y,
+        X_train, X_valid, y_train, y_valid = train_test_split(ts_features, xgb_y,
                                                               stratify=xgb_y)
         params = {
                 'objective': 'multi:softprob',
@@ -28,16 +24,16 @@ class FForma:
 
         dtrain = xgb.DMatrix(data=X_train, label=y_train)
         dvalid = xgb.DMatrix(data=X_valid, label=y_valid)
-        
-        xgb_model = xgb.train(params=params, dtrain=dtrain, 
+
+        xgb_model = xgb.train(params=params, dtrain=dtrain,
                               evals=[(dtrain, 'train'), (dvalid, 'eval')])
 
         # Obtener pesos para el ensamble
-        weights = xgb_model.predict(xgb.DMatrix(ts_feats))
-        self.weights_ = pd.DataFrame(weights, 
+        weights = xgb_model.predict(xgb.DMatrix(ts_features))
+        self.weights_ = pd.DataFrame(weights,
                                      index=errors.index,
                                      columns=errors.columns)
-         
+
         return self
 
     def predict(self, base_preds):
